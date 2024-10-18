4137056eeeb58fc2cf7111716004ca7f6507cfc4
victorlima11 <victorconta1009@gmail.com>
2024-10-18 18:15:44 -0300

    b

--- new_code/aaa.sql
+++ new_code/aaa.sql
@@ -24,6 +24,37 @@ CREATE TABLE usuarios (
     sexo VARCHAR(10) CHECK (sexo IN ('Masculino', 'Feminino')) NOT NULL
 );
 
+
+
+
+-- NOVO:
+
+
+
+CREATE TABLE usuarios (
+    id SERIAL PRIMARY KEY,
+    nome VARCHAR(255) NOT NULL,
+    senha VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    data_nasc DATE NOT NULL,
+    cidade VARCHAR(255) NOT NULL,
+    sexo VARCHAR(10) NOT NULL
+);
+
+
+CREATE TABLE agendamentos (
+    id SERIAL PRIMARY KEY,
+    nome VARCHAR(255) NOT NULL,
+    data DATE NOT NULL,
+    horario TIME NOT NULL,
+    local VARCHAR(255) NOT NULL,
+    descricao VARCHAR(255),
+    usuario_id INT,
+    FOREIGN KEY (usuario_id) REFERENCES usuarios(id) ON DELETE CASCADE
+);
+
+
+
 select * from usuarios
 drop table agendamentos
 select * from agendamentos
