0b9fe1e415b51919e671910cf8dee6ed70062c31
victorlima11 <victorconta1009@gmail.com>
2024-10-28 19:23:19 -0300

    adicionando verificação por email

--- new_code/model/emails.py
+++ new_code/model/emails.py
@@ -1,7 +1,8 @@
 import random
 import smtplib
 from email.mime.text import MIMEText
-from datetime import datetime, timedelta
+from datetime import datetime
+from tkinter import simpledialog
 
 class VerificacaoEmail:
     def __init__(self, email_usuario):
@@ -14,24 +15,25 @@ class VerificacaoEmail:
         self.timestamp = datetime.now()
         return self.codigo
 
+    # Email de Verificação
     def enviar_email(self):
         if self.codigo is None:
             raise ValueError("Código não gerado.")
         
         msg = MIMEText(f"Seu código de verificação é: {self.codigo}")
         msg['Subject'] = 'Código de Verificação'
-        msg['From'] = 'projetodeagendamento@gmail.com'
+        msg['From'] = 'projetodeagendamentos@gmail.com'
         msg['To'] = self.email_usuario
 
-        with smtplib.SMTP('smtp.exemplo.com', 587) as server:
+        with smtplib.SMTP('smtp.gmail.com', 587) as server:
             server.starttls()
-            server.login('projetodeagendamento@gmail.com', 'pythonmuchbetterthanjava')
+            server.login('projetodeagendamentos@gmail.com', 'xaqh roat ndyq ajol')
             server.send_message(msg)
 
+    def solicitar_codigo_verificacao(self):
+        self.gerar_codigo()
+        self.enviar_email()
+        return simpledialog.askinteger("Código de Verificação", "Digite o código enviado para seu e-mail:")
+
     def validar_codigo(self, codigo_inserido):
-        if self.codigo is None or self.timestamp is None:
-            return False
-        
-        if codigo_inserido == self.codigo and (datetime.now() - self.timestamp) < timedelta(minutes=5):
-            return True
-        return False
+        return codigo_inserido == self.codigo
